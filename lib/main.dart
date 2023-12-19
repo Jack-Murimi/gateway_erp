e469e22c003c02f1edb76ce4bf5021bcc4546332
Jack Murimi <jahck31@gmail.com>
2023-12-19 18:31:46 +0300

    main

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:gateway_erp/pages/homepage.dart';
+import 'package:gateway_erp/theme/theme.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+      url: "https://fsogcozrohprhqfilhix.supabase.co",
+      anonKey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzb2djb3pyb2hwcmhxZmlsaGl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI3OTgwMjgsImV4cCI6MjAxODM3NDAyOH0.E_rnV3uupAHaIw3y4YXRjtAfQ9XaFPfSb0NOnsM8Xng");
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/theme/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+  colorScheme: const ColorScheme.light(),
+  appBarTheme: const AppBarTheme(centerTitle: true),
+);
+
+ThemeData darkTheme = ThemeData(
+  colorScheme: const ColorScheme.dark(),
+  appBarTheme: const AppBarTheme(centerTitle: true),
+);
